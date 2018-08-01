c0695ca88a7e0a407bf7ea61f87a5af64a5563c9
NexiPayments <tech.ecommerce@nexi.it>
2018-08-01 17:43:25 +0200

    Aggiornato metodo di pagamento Hosted Fields

--- web-mobile/hosted-fields/php/hosted_fields.php
+++ web-mobile/hosted-fields/php/hosted_fields.php
@@ -1,88 +1,93 @@
 <?php
+$APIKEY = ""; // Sostituire con il valore fornito da Nexi
+$CHIAVESEGRETA = ""; // Sostituire con il valore fornito da Nexi
 
-// Alias e chiave segreta
-$alias = "<ALIAS>"; // Sostituire con il valore fornito da Nexi
-$chiaveSegreta = "<CHIAVE SEGRETA PER CALCOLO MAC>"; // Sostituire con il valore fornito da Nexi
-
-$requestUrl = "https://int-ecommerce.nexi.it/ecomm/ecomm/DispatcherServlet";
-$merchantServerUrl = "https://" . $_SERVER['HTTP_HOST'] . "/xpay/pagamento_semplice/codice_base/";
-
-$codiceTransazione  = "TESTPS_" . date('YmdHis');
-$divisa = "EUR"; 
+$codiceTransazione = "TESTPS_" . date('YmdHis');
+$divisa = "EUR";
 $importo = "5000";
 $timeStamp = (time()) * 1000;
 
 // Calcolo MAC
-$mac = sha1('alias=' . $alias . 'codTrans=' . $codiceTransazione . 'divisa=' . $divisa . 'importo=' . $importo . 'timeStamp=' . $timeStamp . $chiaveSegreta);
+$mac = sha1('apiKey=' . $APIKEY . 'codiceTransazione=' . $codiceTransazione . 'divisa=' . $divisa . 'importo=' . $importo . 'timeStamp=' . $timeStamp . $CHIAVESEGRETA);
 ?>
 
 <html>
     <head>
-        <script type="text/javascript" src="https://int-ecommerce.nexi.it/ecomm/hostedPayments/JavaScript/custom?bundle=HP_FULL&alias=<?php echo $alias; ?>"></script>
+        <script type="text/javascript" src="https://int-ecommerce.nexi.it/ecomm/hostedPayments/JavaScript/custom?bundle=HP_FULL&alias=<?php echo $APIKEY; ?>"></script>
         <script type="text/javascript">
+
             // Configurazione iniziale SDK            
             $(document).ready(function () {
+
                 // 1.1 Inizializzazione SDK
                 XPay.init();
+
                 // 1.2 Impostazione ambiente. Valori ammessi:
-                // XPay.Environments.COLL: collaudo locale
+                // XPay.Environments.INTEG: collaudo
                 // XPay.Environments.PROD: produzione
-                XPay.setEnvironment(XPay.Environments.COLL);
+                XPay.setEnvironment(XPay.Environments.INTEG);
+
                 // 1.3 Configurazione XPay SDK con API Key del merchant
-                XPay.setAPIKey('payment_3444153');
+                XPay.setAPIKey('<?php echo $APIKEY; ?>');
+
                 // 2 Iniezione del calcolo nonce nel processo di submit della form;
                 // NB: l'effettiva implementazione dipende da come il merchant gestisce il submit
 
                 var $form = $('#payment-form');
+
                 $form.find('#pagaBtn').click(function () {
                     // 2.1 Inibizione del click sul bottone di invio della form
                     $(this).prop('disabled', true);
-                    // 2.2 Creazione del nonce e assegnazione dell'handler di gestione della risposta di Xpay; il submit del form verso back-end verrà nell’handler, che deve essere implementato dal merchant
+                    // 2.2 Creazione del nonce e assegnazione dell'handler di gestione della risposta di XPay; il submit del form verso back-end verrà nell’handler, che deve essere implementato dal merchant
                     XPay.createNonce("payment-form", xpayResponseHandler);
                 });
             });
 
             function xpayResponseHandler(response) {
                 console.log(response);
+
                 // Recupero il form
                 var $form = $('#payment-form');
-                if (response.esito && response.esito == "OK") { // nonce creato
+
+                if (response.esito && response.esito === "OK") { // nonce creato
                     // 3.A Recupero del nonce e di altre proprietà in output; inserimento come campi nascosti 
-                    // del form.Il backend dovrà eventualmente validare il mac della risposta
-                    $form.append($('<input type="hidden" name="xpayNonce">').val(response.nonce));
+                    // del form. Il backend dovrà eventualmente validare il mac della risposta
+
+                    $form.append($('<input type="hidden" name="xpayNonce">').val(response.xpayNonce));
                     $form.append($('<input type="hidden" name="xpayIdOperazione" > ').val(response.idOperazione));
-                    $form.append($('<input type="hidden" name="xpayTimeStamp">').val(response.timestamp));
+                    $form.append($('<input type="hidden" name="xpayTimeStamp">').val(response.timeStamp));
                     $form.append($('<input type="hidden" name="xpayEsito">').val(response.esito));
                     $form.append($('<input type="hidden" name="xpayMac">').val(response.mac));
+
                     // Submit del form
                     $form.get(0).submit();
                 } else {
                     // 3.B Visualizzazione errore e ripristino bottone form
-                    $form.find('.payment-error').text("[" + response.errore.codice + "] " +
-                            response.errore.messaggio);
+                    $form.find('.payment-error').text("[" + response.errore.codice + "] " + response.errore.messaggio);
                     $form.find('#pagaBtn').prop('disabled', false);
                 }
-            };
+            }
         </script>
     </head>
     <body>
-        <form action="payment.php" id="payment-form" method="POST">
-            <input type="hidden" data-xpay-order="importo" name="importo" id="importo" value="<?php echo $importo; ?>"/>
-            <input type="hidden" data-xpay-order="timeStamp" name="timeStamp" id="timeStamp" value="<?php echo $timeStamp; ?>"/>
-            <input type="hidden" data-xpay-order="divisa" name="divisa" id="divisa" value="<?php echo $divisa; ?>" />
-            <input type="hidden" data-xpay-order="mac" name="mac" value="<?php echo $mac; ?>" id="mac" /> 
-            <input type="hidden" data-xpay-order="codiceTransazione" name="codiceTransazione" value="<?php echo $codiceTransazione; ?>" id="codiceTransazione" />
-            <input type="hidden" name="alias" value="<?php echo $alias; ?>" id="alias"/>
+        <form action="primo_pagamento.php" id="payment-form" method="POST">
+
+            <input type="hidden" data-xpay-order="importo" name="importo" value="<?php echo $importo; ?>" id="importo">
+            <input type="hidden" data-xpay-order="timeStamp" name="timeStamp" value="<?php echo $timeStamp; ?>" id="timeStamp">
+            <input type="hidden" data-xpay-order="divisa" name="divisa" value="<?php echo $divisa; ?>" id="divisa">
+            <input type="hidden" data-xpay-order="mac" name="mac" value="<?php echo $mac; ?>" id="mac"> 
+            <input type="hidden" data-xpay-order="codiceTransazione" name="codiceTransazione" value="<?php echo $codiceTransazione; ?>" id="codiceTransazione">
+            <input type="hidden" name="alias" value="<?php echo $APIKEY; ?>" id="alias">
 
             <h2>Dati Pagamento</h2>
             <br>
             <span class="payment-error" style="color: red;"></span>
             <br>
-            <label for="_importo" >Importo: &nbsp;</label>
+            <label for="_importo" >Importo:  </label>
             <label id="_importo" ><?php echo $importo; ?></label>
             <br><br>
-            <label for="_nOrdine" >Numero di ordine: &nbsp;</label>
-            <label id="_nOrdine" ><?php echo $codTrans; ?></label>
+            <label for="_nOrdine" >Numero di ordine:  </label>
+            <label id="_nOrdine" ><?php echo $codiceTransazione; ?></label>
             <br><br>
             <label for="_email" >Indirizzo e-mail</label>
             <input id="_email" type="text" >
@@ -96,7 +101,8 @@ $mac = sha1('alias=' . $alias . 'codTrans=' . $codiceTransazione . 'divisa=' . $
             <label for="cvv" >CVV</label>
             <input type="text" maxlength="3" data-xpay-card="cvv" id="cvv">
             <br><br>
-            <input type="button" value="Paga" id="pagaBtn" /> 
+            <input type="button" value="Paga" id="pagaBtn"> 
+
         </form>
     </body>
 </html>
